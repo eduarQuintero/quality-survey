f0e0bca6ba7d34a1787fac2ec84ce31806600ed2
eduar <eduar.curi73@outlook.com>
2023-10-31 11:59:14 -0500

    finished

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,60 @@
 const score = document.getElementsByClassName("container__score")
+const btnSubmit = document.getElementById("submit")
+const principalContainer = document.getElementById("principalContainer")
+const secondaryContainer = document.getElementById("secondaryContainer")
+const finalMessage = document.getElementById("finalMessage")
+
+let scoreValue = 0
 
 score[0].addEventListener("click", () => {
-    console.log("presionaste el numero 1")
-    score[0].classList.toggle("background_selection")
+    scoreValue = score[0].innerText
+    score[1].classList.remove("background_selection")    
+    score[2].classList.remove("background_selection")    
+    score[3].classList.remove("background_selection")    
+    score[4].classList.remove("background_selection")    
+    score[0].classList.add("background_selection")   
+    console.log(scoreValue)
 })
 score[1].addEventListener("click", () => {
-    console.log("presionaste el numero 2")
-    score[1].classList.toggle("background_selection")
+    scoreValue = score[1].innerText
+    score[0].classList.remove("background_selection")    
+    score[2].classList.remove("background_selection")    
+    score[3].classList.remove("background_selection")    
+    score[4].classList.remove("background_selection")    
+    score[1].classList.add("background_selection")   
+    console.log(scoreValue)
 })
 score[2].addEventListener("click", () => {
-    console.log("presionaste el numero 3")
-    score[2].classList.toggle("background_selection")
+    scoreValue = score[2].innerText
+    score[0].classList.remove("background_selection")    
+    score[1].classList.remove("background_selection")    
+    score[3].classList.remove("background_selection")    
+    score[4].classList.remove("background_selection")    
+    score[2].classList.add("background_selection")   
+    console.log(scoreValue)
 })
 score[3].addEventListener("click", () => {
-    console.log("presionaste el numero 4")
-    score[3].classList.toggle("background_selection")
+    scoreValue = score[3].innerText
+    score[0].classList.remove("background_selection")    
+    score[1].classList.remove("background_selection")    
+    score[2].classList.remove("background_selection")    
+    score[4].classList.remove("background_selection")   
+    score[3].classList.add("background_selection")    
+    console.log(scoreValue)
 })
 score[4].addEventListener("click", () => {
-    console.log("presionaste el numero 5")
-    score[4].classList.toggle("background_selection")
+    scoreValue = score[4].innerText
+    score[0].classList.remove("background_selection")    
+    score[1].classList.remove("background_selection")    
+    score[2].classList.remove("background_selection")    
+    score[3].classList.remove("background_selection")    
+    score[4].classList.add("background_selection")    
+    console.log(scoreValue)
+})
+
+btnSubmit.addEventListener("click", () => {
+    // principalContainer.classList.add("hide")
+    finalMessage.innerText = `You selected ${scoreValue} out of 5`
+    principalContainer.style.display="none"
+    secondaryContainer.style.display="flex"
 })
